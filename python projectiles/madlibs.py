7fecf5152b609904e7d56c7c44479a1e55683c4d
Enyii-99 <165850944+Enyii-99@users.noreply.github.com>
2024-04-28 17:33:50 +0100

    Add files via upload

---
+++ python projectiles/madlibs.py
@@ -0,0 +1,5 @@
+time = input("Time in years: ")
+noun = input("Noun: ")
+madlib = f"Once in every {time}, there comes a {noun} with special powers. \nThis {noun}, is viewed as a chosen one."
+
+print(madlib)
